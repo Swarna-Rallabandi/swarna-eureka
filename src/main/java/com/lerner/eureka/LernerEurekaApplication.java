794ee981a9994e581a0cf5618e13653dacdbe050
Swarna R <swarna.rallabandi@github.com>
2026-01-05 10:27:57 +1100

    fixed code smells

--- src/main/java/com/lerner/eureka/LernerEurekaApplication.java
+++ src/main/java/com/lerner/eureka/LernerEurekaApplication.java
@@ -3,10 +3,7 @@ package com.lerner.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import java.util.Date;
-import java.util.Optional;
-import java.util.stream.Collectors; // unused
-import org.springframework.web.client.RestTemplate; // unused
+
 
 @SpringBootApplication
 @EnableEurekaServer
