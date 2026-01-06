2b63f6eb6077ddb772e345d1136b28c9dc608ce2
Swarna Rallabandi <swarna.rallabandi@gmail.com>
2026-01-07 02:49:22 +1100

    bug fix

--- groovy files/6_build_image.groovy
+++ groovy files/6_build_image.groovy
@@ -59,7 +59,7 @@ pipeline {
                 echo "new format : i27-${env.APPLICATION_NAME}-${BUILD_NUMBER}-${BRANCH_NAME}.${env.POM_PACKAGING}"
                 
                 echo "docker build image"
-                sh "docker build --no-cache --build-arg JAR_PATH=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"   
+                docker build --no-cache --build-arg JAR_PATH=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd
                 
                 """
              }
